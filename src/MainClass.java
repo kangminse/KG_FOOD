08248dca702d2566767f018e204919a0a0156f12
kangminse <dogon3645@naver.com>
2023-05-16 14:30:21 +0900

    first commit

---
+++ src/MainClass.java
@@ -0,0 +1,101 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class MenuDTO{
+	private String mName, mType;
+	private int mPrice;
+	public String getmName() {return mName;}
+	public void setmName(String mName) {this.mName = mName;}
+	public String getmType() {return mType;}
+	public void setmType(String mType) {this.mType = mType;}
+	public int getmPrice() {return mPrice;}
+	public void setmPrice(int mPrice) {this.mPrice = mPrice;}
+
+	
+	
+}
+
+
+
+class DB {
+	private int boardId;
+	Connection con;
+	PreparedStatement ps;
+	ResultSet rs;
+	
+	Scanner input = new Scanner(System.in);
+	
+	public DB(int boardId) {
+		this.boardId = boardId;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			//System.out.println("드라이브 로드 성공!");
+			String id = "c##java", pwd="1234";
+			String url = "jdbc:oracle:thin:@localhost:1521/xe";
+			con = DriverManager.getConnection(url, id, pwd);
+			//System.out.println("연결 성공!");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}//DB
+	
+	public void menu(String type) {
+		System.out.println("================================");
+		String sql = "select * from menu where m_type=? and b_id=?";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, type);
+			ps.setInt(2, boardId);
+			
+			rs = ps.executeQuery();//명령문 실행버튼
+			while(rs.next()) {
+				
+				System.out.println(rs.getString("m_type")+"    "+"메뉴:"+rs.getString("m_name")+"     "+"가격:"+rs.getInt("m_price"));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("================================");
+	}//select
+	
+	public void item() {
+		
+		boolean bool = true;
+		while(bool) {
+			System.out.println("1.메인 2.사이드 3.주류 4.뒤로가기");
+			System.out.print("메뉴를 선택해주세요 : ");
+			
+			int num = input.nextInt();
+			
+			switch(num) {
+			case 1:
+				menu("메인");
+				break;
+			case 2:
+				menu("사이드");	
+				break;
+			case 3:
+				menu("주류");	
+				break;
+			case 4:
+				bool=false;
+				break;
+			}
+		}
+	}//item
+}//class DB
+
+public class MainClass {
+	public static void main(String[] args) {
+		DB db = new DB(2);
+		db.item();
+
+	}//Main
+}//MainClass
+
